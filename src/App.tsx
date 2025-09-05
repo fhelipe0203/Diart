e9da36d9d2f12635803a15990faedf9b44d2e565
fhelipe0203 <octaviouriel2002@gmail.com>
2025-09-05 16:32:52 -0300

    prueba

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Index from "./pages/Index";
 import CatalogPage from "./pages/CatalogPage";
 import NotFound from "./pages/NotFound";
 
+
 const queryClient = new QueryClient();
 
 const App = () => (
